aeb4f83c30aa3a22d9d3353807ef41eeda20b21a
Karan Bhargava <kbhargava962@gmail.com>
2026-02-02 23:55:38 +0530

    Time: 7 ms (48.54%) | Memory: 131.4 MB (89.30%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -5,13 +5,15 @@ public:
         int n  = nums.size();
         long long ans = 0;
         int freq = 0;
-        int maxi = *max_element(nums.begin() , nums.end() );
+        int maxi = 0;
+        for(const int & ele:nums)maxi = max(maxi , ele);
+        
         for(int r = 0 ; r<nums.size() ; r++){
             // expand window
             if(nums[r]==maxi)freq++;
             
             // condition satisfy ,ans update , shrink with check
-            while(freq >= k){
+            while(freq == k){
                 ans += n-r ;
                 if(nums[l]==maxi)freq--;
                 l++;
