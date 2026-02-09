f3a43681bff1fa7ced66d05629c45ffdae3bf6de
Karan Bhargava <kbhargava962@gmail.com>
2026-02-09 19:10:33 +0530

    Time: 5 ms (97.1%), Space: 44.2 MB (96.81%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,31 +1,24 @@
 class Solution {
+int Atmost(vector<int>& a , int k){
+    vector<int> mp(a.size()+1,0);
+    int l = 0 ;
+    int once_appear = 0;
+    int ans = 0 ;
+    for(int r = 0 ; r<a.size() ; r++){
+        if(mp[a[r]]++ == 0) once_appear++;
+        while(once_appear>k){
+            if(--mp[a[l]] == 0){
+                once_appear--;
+            }
+            l++;
+        }
+        ans+=r-l+1;
+    }
+    return ans ; 
+}
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        mp.reserve(nums.size());
-        int l   = 0;
-        int ans = 0;
-        for(int r = 0; r<nums.size() ; r++){
-            mp[nums[r]]++;
-            while(mp.size()>k){
-                mp[nums[l]]--;
-                if(mp[nums[l]]==0)mp.erase(nums[l]);
-                l++;
-            }
-            ans += r-l+1;
-        }
-        l=0;
-        mp.clear();
-        for(int r = 0; r<nums.size() ; r++){
-            mp[nums[r]]++;
-            while(mp.size()>k-1){
-                mp[nums[l]]--;
-                if(mp[nums[l]]==0)mp.erase(nums[l]);
-                l++;
-            }
-            ans -= r-l+1;
-        }
-        return ans ;
+        return Atmost(nums , k) - Atmost(nums , k-1);
     }
     
 };
