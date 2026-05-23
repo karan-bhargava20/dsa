74ca50186cd9d16b043defd4a2108f239c69dc14
Karan Bhargava <kbhargava962@gmail.com>
2026-05-23 11:58:30 +0530

    Time: 464 ms (54.79%) | Memory: 155.4 MB (45.53%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -6,8 +6,8 @@ public:
 
         vector<vector<int>>prefix(m,vector<int>(n,0));
         for(int i = 0; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                prefix[i][j] = (j != 0) ? prefix[i][j - 1] + matrix[i][j]: matrix[i][j];
+            for(int j = 1 ; j < n ; j++){
+                matrix[i][j] +=  matrix[i][j - 1] ;
             }
         }
         int cnt = 0;
@@ -17,7 +17,7 @@ public:
                 unordered_map<int,int>map;
                 map[0] = 1;
                 for(int row = 0; row < m ; row++){
-                    sumPrefix += (l>0) ? prefix[row][r] - prefix[row][l-1] : prefix[row][r];
+                    sumPrefix += (l>0) ? matrix[row][r] - matrix[row][l-1] : matrix[row][r];
                     if(map.count(sumPrefix-target)){
                     cnt += map[sumPrefix-target];
                     }
