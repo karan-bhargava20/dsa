14511419f7a082cc43c149e603b9eef90edd138c
Karan Bhargava <kbhargava962@gmail.com>
2026-05-28 21:56:46 +0530

    Time: 4 ms (52.54%) | Memory: 92.5 MB (58.54%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -3,13 +3,13 @@ public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         vector<int>prefix(arr.size(),0);
         for(int i = 0 ; i<arr.size() ; i++){
-            prefix[i] = (i==0) ? arr[0]:prefix[i-1]^arr[i];
+            prefix[i] = (i!=0) ? prefix[i-1] ^ arr[i] : arr[0] ;
         }
         vector<int>ans;
         for(int row = 0 ; row <queries.size() ; row++){
             int L_i = queries[row][0];
             int R_i = queries[row][1];
-            int xor_i =(L_i==0) ? prefix[R_i] : prefix[R_i] ^ prefix[L_i-1];
+            int xor_i =(L_i!=0) ? prefix[L_i-1] ^ prefix[R_i] : prefix[R_i] ;
             ans.push_back(xor_i);
         }
         return ans ; 
