824924335debd2975e5de382ae2316286044388f
Karan Bhargava <kbhargava962@gmail.com>
2026-05-29 12:15:31 +0530

    Time: 4 ms (52.66%), Space: 92.4 MB (58.5%) - LeetHub - Time: O(N + Q), Space: O(?)

---
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int>prefix(arr.size(),0);
+        for(int i = 0 ; i<arr.size() ; i++){
+            prefix[i] = (i!=0) ? prefix[i-1] ^ arr[i] : arr[0] ;
+        }
+        vector<int>ans;
+        for(int row = 0 ; row <queries.size() ; row++){
+            int L_i = queries[row][0];
+            int R_i = queries[row][1];
+            int xor_i =(L_i!=0) ? prefix[L_i-1] ^ prefix[R_i] : prefix[R_i] ;
+            ans.push_back(xor_i);
+        }
+        return ans ; 
+    }
+};  
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
