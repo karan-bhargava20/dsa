3012c9d7c223e6524c37eed4448e8db74665399e
Karan Bhargava <kbhargava962@gmail.com>
2026-05-29 10:45:33 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (56.11%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         long long xorAll = 0;
-        for(int i = 0 ; i<nums.size() ; i++){
+        for (int i = 0; i < nums.size(); i++) {
             xorAll ^= nums[i];
         }
-        long long diff = xorAll & -xorAll ;
-        int  first = 0 , second = 0;
-        for(int i = 0 ; i<nums.size() ; i++){
-            if(diff & nums[i]){
+        long long diff = xorAll & -xorAll;
+        int first = 0, second = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (diff & nums[i]) {
                 first ^= nums[i];
-            } else{
+            } else {
                 second ^= nums[i];
             }
         }
-        return {first,second};
+        return {first, second};
     }
 };
