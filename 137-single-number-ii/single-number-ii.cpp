e6ebe68b2182a7b4f724f4bb6c4a77124fbb9faf
Karan Bhargava <kbhargava962@gmail.com>
2026-06-12 00:35:35 +0530

    Time: 3 ms (30.34%) | Memory: 13.4 MB (63.21%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>hash;
-        int ans = 0;
-        for(auto e : nums){
-            hash[e]++;
-        }
-        for(auto it : hash){
-            if( it.second == 1 ){
-                ans = it.first;
+    //     unordered_map<int,int>hash;
+    //     int ans = 0;
+    //     for(auto e : nums){
+    //         hash[e]++;
+    //     }
+    //     for(auto it : hash){
+    //         if( it.second == 1 ){
+    //             ans = it.first;
+    //         }
+    //     }
+    //     return ans;
+    // 4ms Beats13 .65%
+    // int maxi = accumulate(nums.begin(), nums.end(), 0);
+    // int L_bit = -1;
+    // while(maxi){
+    //     L_bit++;
+    //     maxi = maxi>>1;
+    // }
+    int ans = 0 ;
+    for(int bit = 0 ; bit <= 31 ; bit++ ){
+        int cnt = 0;
+        for(auto it : nums){
+            if(it & (1 << bit)){
+                cnt++;
             }
         }
-        return ans;
+        if(cnt % 3 == 1){
+            ans |= 1 << bit;
+        }
+
+    }
+    return ans;
     }
 };
 
