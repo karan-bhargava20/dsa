29ef6996d837b4ac7da3cf4d9ac041643195d567
Karan Bhargava <kbhargava962@gmail.com>
2026-06-21 09:59:52 +0530

    Time: 0 ms (100%), Space: 55.1 MB (62.17%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        
+        int n = nums.size();
+        for(int i = 0 ; i < n  ; i++){
+            if(!( 1 <= nums[i] && nums[i] <= n )){
+                nums[i] = n+1;
+          }
+        }
+        for(int i = 0 ; i < n ; i++){
+            int num = abs(nums[i]);
+            if(num == n+1){
+                continue;
+            }
+            int corr_idx = num - 1;
+            if(nums[corr_idx] > 0 ){
+                nums[corr_idx] = -nums[corr_idx];
+            }  
+        }
+        for(int i = 0 ; i < n ; i++){
+            if( nums[i] > 0 ){
+                return i+1;
+            }
+        }
+        return n + 1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
